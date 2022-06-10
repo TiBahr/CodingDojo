e771b45da11fb16fae0213a90fec7169459404d6
ssw <stefan.schwennecker@opitz-consulting.com>
2022-06-10 09:34:44 +0200

    clean Repo

--- backend/application/resources/intogen/intogen.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-intogen = Blueprint("intogen", __name__)
-
-
-@intogen.route("/home")
-@intogen.route("/")
-def course_insights_home():
-    return "intogen home"
--- backend/application/resources/study_soon/study_soon.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-study_soon = Blueprint("study_soon", __name__)
-
-
-@study_soon.route("/home")
-@study_soon.route("/")
-def course_insights_home():
-    return "Study Soon home"
